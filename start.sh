d4ab3d127d6236257b83ef768f3645626ef8cfca
Marcus Perrout <contato@perrout.com.br>
2020-11-03 15:35:12 -0300

    Added start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo Uploading Application container 
+docker-compose up -d
+
+echo Copying the configuration example file
+docker exec -it test-app cp .env.example .env
+
+echo Install dependencies
+docker exec -it test-app composer install
+
+echo Generate key
+docker exec -it test-app php artisan key:generate
+
+echo Make migrations
+docker exec -it test-app php artisan migrate
+
+echo Make seeds
+docker exec -it test-app php artisan db:seed
+
+echo Information of new containers
+docker ps -a
